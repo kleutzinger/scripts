5902f3124b79b1187e8efffa1cc7891948397f45
kleutzinger <6435727+kleutzinger@users.noreply.github.com>
2020-11-26 08:15:41 -0800

    rainwave, super key

---
+++ bind_super.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# bind super to alt_f1
+# startup with this, probably
+xcape -e 'Super_L=Alt_L|F1'
--- file-select.py
+++ file-select.py
@@ -1,9 +1,8 @@
-#!/usr/bin/python3
-
+#!/usr/bin/env python3
 import sys
 import subprocess
 
 encoding = sys.stdout.encoding
-file_path = subprocess.check_output('zenity --file-selection', shell=1).strip()
+file_path = subprocess.check_output("zenity --file-selection", shell=1).strip()
 file_path = file_path.decode(encoding)
 print(file_path)

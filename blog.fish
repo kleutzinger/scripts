1c7085ba26ce8ddc106808fef92c1ef880b38a04
kleutzinger <6435727+kleutzinger@users.noreply.github.com>
2021-01-22 15:16:57 -0800

    add check-notes; wmctrl experiments

--- blog.fish
+++ blog.fish
@@ -1,18 +1,16 @@
 #!/usr/bin/env fish
 # start a lektor instance and open browser
 set BLOG_DIR ~/lektor/kev-project-lektor
-set PORT 9001
+set LEKTOR_PORT 9001
 # ping cannot check ports, use curl
-curl localhost:9001 -s > /dev/null
+curl localhost:$LEKTOR_PORT -s > /dev/null
 if test $status -eq 0
     echo "already running, opening new tab"
-    chromium "http://localhost:$PORT"
+    chromium "http://localhost:$LEKTOR_PORT"
     exit 0
 end
 
 # start up server
 cd $BLOG_DIR
 source ../.venv/bin/activate.fish
-cat ../.venv/bin/activate.fish
-python -m pip -V
-lektor server -p $PORT --browse
+lektor server -p $LEKTOR_PORT --browse
--- window-control.fish
+++ window-control.fish
@@ -1,7 +1,9 @@
 #! /usr/bin/env fish
 # run simple window manager commands on active window
+# moving is really too slow and buggy to rely on
+# perhaps I should convert this file to `.sh`?
+set valid_args 'max' 'min' 'move_next' 'move_prev'
 
-set valid_args 'max' 'min'
 
 if not contains $argv[1] $valid_args
     set msg (string join ', ' $valid_args)
@@ -16,6 +18,25 @@ end
 
 if test $argv[1] = 'min'
     xdotool windowminimize (xdotool getactivewindow)
+    exit 0
+end
+
+if test $argv[1] = 'move_next'
+    set MAXD ( wmctrl -d | tail -n 1 | awk '{print $1}' )
+    set CURD ( wmctrl -d | awk '{ if ($2 == "'\*'") print $1}' )
+    set TOD ( math "($CURD + 1) % ($MAXD + 1)" )
+    wmctrl -r :ACTIVE: -t $TOD; wmctrl -s $TOD
+    exit 0
+end
+
+if test $argv[1] = 'move_prev'
+    set MAXD ( wmctrl -d | tail -n 1 | awk '{print $1}' )
+    set CURD ( wmctrl -d | awk '{ if ($2 == "'\*'") print $1}' )
+    set TOD ( math "($CURD - 1) % ($MAXD + 1)" )
+    if test $TOD -eq "-1"
+        set TOD $MAXD
+    end
+    wmctrl -r :ACTIVE: -t $TOD; wmctrl -s $TOD
     exit 0
 end
 
